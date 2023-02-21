826828e3b8fc0293f90412fe491a3b02d5676357
Bradley Yeo <yeo.bradley@gmail.com>
2023-02-22 07:57:20 +0800

    use vite

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/bradleyyeo/Desktop/COMM253/bradley-korea-comm253/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/bradleyyeo/Desktop/COMM253/bradley-korea-comm253/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/bradleyyeo/Desktop/COMM253/bradley-korea-comm253/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/bradleyyeo/Desktop/COMM253/bradley-korea-comm253/src/css/custom.css'),
+];
--- README.md
+++
@@ -1,2 +0,0 @@
-# bradley-korea-comm253
-This website shows my reflection for my COMM253 Korea Trip
